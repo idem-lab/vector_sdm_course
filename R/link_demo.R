ef18037414b4e689f64f9c02aaefd14cf6d48d46
geryan <geryan@users.noreply.github.com>
2025-06-19 14:47:55 +1000

    add hand calc link demo

--- R/link_demo.R
+++ R/link_demo.R
@@ -23,3 +23,64 @@ predictor_values %>%
       trange * coefs$trange,
     prediction_response = 1/(1+exp(-prediction_link))
   )
+
+
+
+# calculate by hand
+
+# our data
+tseas <- data_pa_random$tseas
+tmax <- data_pa_random$tmax
+trange <- data_pa_random$trange
+#presence <- data_pa_random$presence
+
+# some coefficients/ slopes - play around with these
+beta_tseas <- 0.001
+beta_tmax <- 0.002
+beta_trange <- 0.1
+alpha <- -4
+
+# calculate probability on link scale
+gamma <- alpha + beta_tseas*tseas + beta_tmax*tmax + beta_trange*trange
+
+# logit link function
+logitfun <- function(gamma){
+  1/(1+exp(-gamma))
+}
+
+# calculate probability of presence
+logitfun(gamma)
+
+# calculate a partial response curve
+tseas_mean <- mean(data_pa_random$tseas)
+tmax_mean <- mean(data_pa_random$tmax)
+trange_mean <- mean(data_pa_random$trange)
+
+pr_tmax_data <- seq(
+  from = 20,
+  to = 40,
+  by = 0.2
+)
+
+# some coefficients/ slopes - play around with these
+beta_tseas <- 0.001
+beta_tmax <- 0.2
+beta_trange <- 0.1
+alpha <- -5
+
+# calculate probability on link scale
+gamma_pr <- alpha + beta_tseas*tseas_mean + beta_tmax*pr_tmax_data + beta_trange*trange_mean
+
+# logit link function
+logitfun <- function(gamma){
+  1/(1+exp(-gamma))
+}
+
+# calculate probability of presence
+prob_pr <- logitfun(gamma_pr)
+
+plot(
+  x = pr_tmax_data,
+  y = prob_pr,
+  ylim = c(0, 1)
+)
